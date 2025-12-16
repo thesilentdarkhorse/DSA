1b78c93c47872786b1c91614be4e6efe224b710f
thesilentdarkhorse <thesilentdarkhorse@gmail.com>
2025-12-16 18:56:46 +0530

    Question4 (NL) done

---
+++ Question4-NL.py
@@ -0,0 +1,11 @@
+
+'''
+Q1.
+Given a list of integers, print the sum of all elements.
+
+nums = [3, 7, 1, 9, 2]
+# output: 22
+'''
+from typing import List
+def Solution(nums:List[int]):
+    return(sum(nums))
